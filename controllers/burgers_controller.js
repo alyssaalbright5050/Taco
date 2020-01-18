ed34f6ad23c35ee76c068e0ea9c13268b015b5e1
Alyssa Albright <alyssaalbright5050@gmail.com>
2020-01-17 19:52:39 -0600

    connection and orm

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,34 @@
+// Dependencies
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+	burger.selectAll(function(data) {
+		var dbObject = {
+			burgers: data
+		};
+
+		res.render("index", dbObject);
+	});
+});
+
+router.post("/", function(req, res) {
+	console.log(req.body.burger_add);
+	if(req.body.burger_name !== "") {
+		burger.insertOne(req.body.burger_add.trim(), function() {
+			res.redirect("/");
+		});
+	}
+});
+
+router.put("/:id", function(req, res) {
+	console.log(req.params.id);
+
+	burger.updateOne(req.params.id, function() {
+		res.redirect("/");
+	});
+})
+
+// Export
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+
+INSERT INTO burgers (burger_name) VALUES ('Brisquet Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Burger');
