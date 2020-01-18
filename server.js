63ac72c6fd5dcff2324b53d75a49fc4e83a4ed61
Alyssa Albright <alyssaalbright5050@gmail.com>
2020-01-18 10:04:33 -0600

    Added image

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static('images'));
 
 // Set up rendering engine, Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
