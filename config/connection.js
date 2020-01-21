8043b4f78ab86a43afe8a300b346b669ee86b0bd
Alyssa Albright <alyssaalbright5050@gmail.com>
2020-01-20 18:10:46 -0600

    added info for Heroku

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
   port: 3306,
-  host: "localhost",
+  host: "mysql://hguasfwalyv13rb2:bmwbbmk73dak5ket@tj5iv8piornf713y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/nnfdmq3uyaou6uh9",
   user: "root",
   password: "Fish2015!",
   database: "burger_db"
